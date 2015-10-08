cbb64b1fa05d9245606275c35d345e6ca70253f5
Mattia46 <mattia.assogna@gmail.com>
2015-10-08 20:23:58 +0100

    Done

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,12 @@
 def fizzbuzz(number)
-  'fizz'
-  'buzz'
+  if number % 15 == 0
+    return 'fizzbuzz'
+  elsif number % 5 == 0
+    return 'buzz'
+  elsif number % 3 == 0
+  return 'fizz'
+else
+  return number
+  end
 
 end
